002511d260589c7660d376969f9e6d0728576b3b
Rahul <rahulpatil07109@gmail.com>
2022-02-06 16:36:24 +0530

    Done with the home page and responsiveness

--- script.js
+++ script.js
@@ -1,14 +1,25 @@
+let hamburgerOut = document.getElementById("bar1");
+let hamburgerIn = document.getElementById("bar2");
+let navbar = document.getElementById("navbar");
+let header = document.getElementById("header");
 // Nav bar part
-let navbar = document.getElementById("header");
-
 let lastScrollTop = 0;
 
 window.addEventListener("scroll", () => {
   let currScrollTop = window.pageYOffset || document.documentElement.scrollTop;
   if (currScrollTop > lastScrollTop) {
-    navbar.style.top = "-85.5px";
+    header.style.top = "-85.5px";
   } else {
-    navbar.style.top = "0";
+    header.style.top = "0";
   }
   lastScrollTop = currScrollTop;
+});
+
+// Nav bar Mobile (Hamburger)
+
+hamburgerOut.addEventListener("click", () => {
+  navbar.classList.toggle("active");
+});
+hamburgerIn.addEventListener("click", () => {
+  navbar.classList.toggle("active");
 });
