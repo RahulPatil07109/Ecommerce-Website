3b2bda2ef68248c5fe5c874b3ed6ea327c469462
Rahul <rahulpatil07109@gmail.com>
2022-02-07 14:57:51 +0530

    Done with the shop page + Responsiveness

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
+let MainImg = document.getElementById("MainImg");
+let smallImg = document.getElementsByClassName("small-img");
+
 let hamburgerOut = document.getElementById("bar1");
 let hamburgerIn = document.getElementById("bar2");
+
 let navbar = document.getElementById("navbar");
 let header = document.getElementById("header");
+
 // Nav bar part
 let lastScrollTop = 0;
 
@@ -23,3 +28,18 @@ hamburgerOut.addEventListener("click", () => {
 hamburgerIn.addEventListener("click", () => {
   navbar.classList.toggle("active");
 });
+
+// Single page
+
+smallImg[0].onclick = () => {
+  MainImg.src = smallImg[0].src;
+};
+smallImg[1].onclick = () => {
+  MainImg.src = smallImg[1].src;
+};
+smallImg[2].onclick = () => {
+  MainImg.src = smallImg[2].src;
+};
+smallImg[3].onclick = () => {
+  MainImg.src = smallImg[3].src;
+};
